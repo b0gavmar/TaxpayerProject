4c36c6191124820bd180c25b28ea9ab65a519ab9
2020b Gavlik Martin <b0gavmar@vasvari.org>
2025-03-27 14:15:07 +0100

    modell teszt

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using TaxpayerProject.Models;
+
+try
+{
+    Taxpayer ures = new Taxpayer("üres vagyok", "");
+}catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+Taxpayer antal = new Taxpayer("Adózó Antal", "adozo.antal@ado.hu");
+try
+{
+    antal.IncreaseTaxCredit(-2000);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+antal.IncreaseTaxCredit(2000);
+antal.DecreaseTaxCredit(1000);
+Console.WriteLine(antal);
